31227da5b1cf44c646cbde7247bda9679b08c4ca
RaK0 <pawel.wytrzyszczewski@o2.pl>
2023-08-23 18:51:02 +0200

    dodano slidercontrollera z postem updejtem i getem

---
+++ NGSBlazor/Server/Controllers/v1/SliderController.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NGSBlazor.Shared.Constants.Permission;
+using NGSBlazor.Shared.Features.Slider.Commands;
+using NGSBlazor.Shared.Features.Slider.Queries;
+
+namespace NGSBlazor.Server.Controllers.v1
+{
+    public class SliderController : BaseApiVersionController<Slider>
+    {
+        [Authorize(Policy = Permissions.Slider.Add)]
+        [HttpPost]
+        public async Task<IActionResult> Post(AddSliderCommand addSlider)
+        {
+            return Ok(await _mediator.Send(addSlider));
+        }
+
+        [HttpPatch] 
+        public async Task<IActionResult> Update(EditSliderCommand editSlider) 
+        {
+            return Ok(await _mediator.Send(editSlider));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _mediator.Send(new GetAllSliderQuery()));
+        }
+    }
+}
