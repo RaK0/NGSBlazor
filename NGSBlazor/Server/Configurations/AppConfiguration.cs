89c487beba73e309ae73de06b7d0a369ad6e6cec
RaK0 <pawel.wytrzyszczewski@o2.pl>
2023-08-23 19:06:39 +0200

    dodano required

--- NGSBlazor/Server/Configurations/AppConfiguration.cs
+++ NGSBlazor/Server/Configurations/AppConfiguration.cs
@@ -2,9 +2,7 @@
 {
     public class AppConfiguration
     {
-#pragma warning disable CS8618 // Pole niedopuszczające wartości null musi zawierać wartość inną niż null podczas kończenia działania konstruktora. Rozważ zadeklarowanie pola jako dopuszczającego wartość null.
-        public string JWTSecret { get; set; }
-#pragma warning restore CS8618 // Pole niedopuszczające wartości null musi zawierać wartość inną niż null podczas kończenia działania konstruktora. Rozważ zadeklarowanie pola jako dopuszczającego wartość null.
-        public bool Debug { get; set; }
+        public required string JWTSecret { get; set; }
+        public required bool Debug { get; set; }
     }
 }
