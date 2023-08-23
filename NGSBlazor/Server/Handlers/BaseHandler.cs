cb79f96aeb81aed13970c45c773ce6a300851ea0
RaK0 <pawel.wytrzyszczewski@o2.pl>
2023-08-23 18:51:55 +0200

    dodano bazowy handler dla mediatora

---
+++ NGSBlazor/Server/Handlers/BaseHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Localization;
+using NGSBlazor.Server.Interfaces.Repositories;
+
+namespace NGSBlazor.Server.Handlers
+{
+    internal abstract class BaseHandler<TKey, LocalizeSource> where LocalizeSource : class
+    {
+        protected readonly IMapper _mapper;
+        protected readonly IStringLocalizer<LocalizeSource> _localizer;
+        protected readonly IUnitOfWork<TKey> _unitOfWork;
+
+        protected BaseHandler(IMapper mapper, IStringLocalizer<LocalizeSource> localizer, IUnitOfWork<TKey> unitOfWork)
+        {
+            _mapper = mapper;
+            _localizer = localizer;
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
