a9b43e3bd2d741c552e3fd937b0c54479106a64a
RaK0 <pawel.wytrzyszczewski@o2.pl>
2023-08-23 18:50:39 +0200

    dodano bazowy kontroler api z mediatorem i logerem

---
+++ NGSBlazor/Server/Controllers/BaseApiVersionController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NGSBlazor.Server.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public abstract class BaseApiVersionController<T> : ControllerBase
+    {
+        private ILogger<T>? _loggerInstance;
+        protected ILogger<T> _logger => _loggerInstance ??= HttpContext.RequestServices.GetService<ILogger<T>>();
+
+        private IMediator? _mediatorInstance;
+        protected IMediator _mediator => _mediatorInstance ??= HttpContext.RequestServices.GetService<IMediator>();
+
+    }
+}
