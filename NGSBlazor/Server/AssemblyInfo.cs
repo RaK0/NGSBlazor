dc438ab7930c41afb3742570c8bf1da27af79e65
RaK0 <pawel.wytrzyszczewski@o2.pl>
2023-08-23 18:56:08 +0200

    dodano mozliwosc uzycia klas z internal w klasach testowych

---
+++ NGSBlazor/Server/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// W projektach w stylu zestawu SDK, takich jak ten, kilka atrybutów zestawu, kiedyś
+// definiowanych w tym pliku, jest teraz automatycznie dodawanych podczas kompilacji
+// i wypełnianych wartościami zdefiniowanymi we właściwościach projektu. Aby uzyskać
+// szczegółowe informacje o tym, które atrybuty są dołączane i jak dostosować ten
+// proces, zobacz: https://aka.ms/assembly-info-properties
+
+
+// Ustawienie atrybutu ComVisible na wartość false sprawia, że typy w tym zestawie
+// nie są widoczne dla składników modelu COM. Jeśli chcesz uzyskać dostęp do typu
+// w tym zestawie z modelu COM, ustaw w tym typie atrybut ComVisible na wartość true.
+
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem elementu typelib w przypadku
+// udostępnienia tego projektu w modelu COM.
+
+[assembly: Guid("a4e51845-97b6-4c60-b60c-e6fbecbbce69")]
+[assembly: InternalsVisibleTo("Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7")]
