3c29bbd58393b0f3c0643f7acdc3c08d540765d1
RaK0 <pawel.wytrzyszczewski@o2.pl>
2023-08-23 18:53:35 +0200

    poprawiono nazwy metod dodano async do metod async

--- NGSBlazor/Server/Repositories/UnitOfWork.cs
+++ NGSBlazor/Server/Repositories/UnitOfWork.cs
@@ -2,6 +2,7 @@
 using Infrastructure.Interfaces.Entities;
 using Microsoft.EntityFrameworkCore;
 using NGSBlazor.Server.Interfaces.Repositories;
+using System.Collections;
 
 namespace NGSBlazor.Server.Repositories
 {
@@ -23,20 +24,20 @@ namespace NGSBlazor.Server.Repositories
                 Type repositoryType = typeof(RepositoryAsync<,>).MakeGenericType(type, typeof(TKey));
                 object? repositoryInstance = Activator.CreateInstance(repositoryType, _context);
                 if(repositoryInstance != null)
-                    _repositories[type] = repositoryInstance;
+                    _repositories[type] = repositoryInstance;                
             }
             return (IRepositoryAsync<T, TKey>)_repositories[type];
         }
-        public async Task<int> Commit(CancellationToken cancellationToken)
+        public async Task<int> CommitAsync(CancellationToken cancellationToken)
         {
             return await _context.SaveChangesAsync(cancellationToken);
         }
 
-        public Task<int> CommitAndRemoveCache(CancellationToken cancellationToken, params string[] cacheKeys)
+        public Task<int> CommitAndRemoveCacheAsync(CancellationToken cancellationToken, params string[] cacheKeys)
         {
             throw new NotImplementedException();
         }       
-        public Task Rollback()
+        public Task RollbackAsync()
         {
             _context.ChangeTracker.Entries().ToList().ForEach(x => x.Reload());
             return Task.CompletedTask;
